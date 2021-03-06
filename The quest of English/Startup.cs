fc0b1456853492e047f19ca7b280ee37782551df
Kurczak1233 <kurczak12333@gmail.com>
2021-03-06 21:46:06 +0100

    Create unfinished fixed-top class of navabar

--- The quest of English/Startup.cs
+++ The quest of English/Startup.cs
@@ -27,6 +27,7 @@ namespace The_quest_of_English
         {
             services.AddControllersWithViews();
             services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer("Server=FLUTTERSHY\\SQLEXPRESS;Database=TheQuestOfEnglish;Trusted_Connection=True;"));
+            services.AddRazorPages().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
