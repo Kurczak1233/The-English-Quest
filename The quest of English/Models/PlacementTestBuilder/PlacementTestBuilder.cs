94444c7bafa86e828f57833ae0b9f464d95e2cfe
Kurczak1233 <kurczak12333@gmail.com>
2021-03-21 21:37:01 +0100

    Prepare builder desing pattern

---
+++ The quest of English/Models/PlacementTestBuilder/PlacementTestBuilder.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace The_quest_of_English.Models.PlacementTestBuilder
+{
+    public class PlacementTestBuilder
+    {
+        private PlacementTestQuestion _question = new PlacementTestQuestion();
+        public void AddFirstPartOfQuestion(string firstPart)
+        {
+            _question.QuestionFirstPart = firstPart;
+        }
+        public void AddMarkedPartOfQuestion(string decoratedPart)
+        {
+            _question.QuestionDecoratedPart = decoratedPart;
+        }
+        public void AddSecondPartOfQuestion(string secondPart)
+        {
+            _question.QuestionSecondPart = secondPart;
+        }
+        public void AddFirstAnswear(string answear)
+        {
+            _question.FirstAnswear = answear;
+        }
+        public void AddSecondAnswear(string answear)
+        {
+            _question.SecondAnswear = answear;
+        }
+        public void AddThirdAnswear(string answear)
+        {
+            _question.ThirdAnswear = answear;
+        }
+        public void AddFourthAnswear(string answear)
+        {
+            _question.FourthAnswear = answear;
+        }
+        public void AddCorrectAnswear(string answear)
+        {
+            _question.CorrectAnswear = answear;
+        }
+    }
+}
---
+++ The quest of English/Models/PlacementTestBuilder/PlacementTestQuestion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace The_quest_of_English.Models
+{
+    public class PlacementTestQuestion
+    {
+        public int Points { get; set; }
+        public int QuestionNumber { get; set; }
+        public string QuestionFirstPart { get; set; }
+        public string QuestionDecoratedPart { get; set; }
+        public string QuestionSecondPart { get; set; }
+        public string FirstAnswear { get; set; }
+        public string SecondAnswear { get; set; }
+        public string ThirdAnswear { get; set; }
+        public string FourthAnswear { get; set; }
+        public string CorrectAnswear { get; set; }
+        
+    }
+}
