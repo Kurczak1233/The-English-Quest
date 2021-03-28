e7dd910f51a9f5c1f620a2433a13650691a81bde
Kurczak1233 <kurczak12333@gmail.com>
2021-03-28 16:44:17 +0200

    Added controllers and views

---
+++ The quest of English/Areas/Admin/Controllers/GrammarController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace The_quest_of_English.Areas.Admin.Controllers
+{
+    public class GrammarController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult FCE()
+        {
+            return View();
+        }
+        public IActionResult CAE()
+        {
+            return View();
+        }
+        public IActionResult CPE()
+        {
+            return View();
+        }
+    }
+}
---
+++ The quest of English/Areas/Admin/Controllers/WritingController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace The_quest_of_English.Areas.Admin.Controllers
+{
+    public class WritingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
