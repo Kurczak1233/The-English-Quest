1c18509d69e430b836705f7cc9e2d72fba1d9b17
Kurczak1233 <kurczak12333@gmail.com>
2021-04-01 18:43:13 +0200

    Created quizes sections

--- The quest of English/Models/BaseModels/QuizViewModel.cs
+++ The quest of English/Models/BaseModels/QuizViewModel.cs
@@ -6,6 +6,8 @@ namespace The_quest_of_English
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string UserId { get; set; }
+
         public ApplicationUserViewModel User { get; set; }
     }
 }
--- TheEnglishQuestCore/DTOs/BaseDto/QuizDto.cs
+++ TheEnglishQuestCore/DTOs/BaseDto/QuizDto.cs
@@ -6,6 +6,7 @@ namespace TheEnglishQuestCore
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string UserId { get; set; } 
         public ApplicationUserDto User { get; set; }
     }
 }
