9161a8566a43756c7adb5192562273af228edb02
Kurczak1233 <kurczak12333@gmail.com>
2021-04-02 00:01:47 +0200

    Updated Tasks model

--- TheQuestOfEnglishDatabase/Entities/PlacementTestTask.cs
+++ TheQuestOfEnglishDatabase/Entities/PlacementTestTask.cs
@@ -6,15 +6,11 @@ namespace TheEnglishQuestDatabase
     {
         [Key]
         public int Id { get; set; }
-        [Required]
         public string QuestionFirstPart { get; set; }
         public string QuestionDecoratedPart { get; set; }
         public string QuestionSecondPart { get; set; }
-        [Required]
         public string FirstAnswear { get; set; }
-        [Required]
         public string SecondAnswear { get; set; }
-        [Required]
         public string ThirdAnswear { get; set; }
         public string FourthAnswear { get; set; }
         [Required]
---
+++ TheQuestOfEnglishDatabase/Migrations/20210401220100_removeRequiredRestrictionFromTasks.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TheQuestOfEnglishDatabase.Migrations
+{
+    public partial class removeRequiredRestrictionFromTasks : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ThirdAnswear",
+                table: "PlacementTestTasks",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SecondAnswear",
+                table: "PlacementTestTasks",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "QuestionFirstPart",
+                table: "PlacementTestTasks",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstAnswear",
+                table: "PlacementTestTasks",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ThirdAnswear",
+                table: "PlacementTestTasks",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SecondAnswear",
+                table: "PlacementTestTasks",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "QuestionFirstPart",
+                table: "PlacementTestTasks",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstAnswear",
+                table: "PlacementTestTasks",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
