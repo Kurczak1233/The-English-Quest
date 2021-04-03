5e2d824bfcba394242643f26bbc8f189e16c7c0e
Kurczak1233 <kurczak12333@gmail.com>
2021-04-03 17:32:58 +0200

    Implemented Reading part

--- The quest of English/Models/ReadingViewModels/ReadingQuizViewModel.cs
+++ The quest of English/Models/ReadingViewModels/ReadingQuizViewModel.cs
@@ -4,7 +4,7 @@ namespace The_quest_of_English
 {
     public class ReadingQuizViewModel : QuizViewModel
     {
-        public IEnumerable<GrammarTasksViewModel> ReadingTasks { get; set; }
+        public IEnumerable<ReadingTaskViewModel> ReadingTasks { get; set; }
         public string Level { get; set; }
         public string Section { get; set; } = "Reading";
     }
