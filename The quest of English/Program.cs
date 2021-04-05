ee1c5308715ad1e1711768b6d4bb359a01a9ec85
Kurczak1233 <kurczak12333@gmail.com>
2021-04-05 10:52:21 +0200

    Fixed bug

--- The quest of English/Program.cs
+++ The quest of English/Program.cs
@@ -1,12 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Azure.Identity;
 
 namespace The_quest_of_English
 {
@@ -21,10 +14,6 @@ namespace The_quest_of_English
             Host.CreateDefaultBuilder(args)
 .ConfigureAppConfiguration((context, config) =>
 {
-var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
-config.AddAzureKeyVault(
-keyVaultEndpoint,
-new DefaultAzureCredential());
 })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
