4cdb546867c834044c38d41e3b6aa2258310e9bc
Kurczak1233 <kurczak12333@gmail.com>
2021-04-05 10:51:27 +0200

    Add do Github

--- The quest of English/Program.cs
+++ The quest of English/Program.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Azure.Identity;
 
 namespace The_quest_of_English
 {
@@ -18,6 +19,13 @@ namespace The_quest_of_English
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+.ConfigureAppConfiguration((context, config) =>
+{
+var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
+config.AddAzureKeyVault(
+keyVaultEndpoint,
+new DefaultAzureCredential());
+})
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
